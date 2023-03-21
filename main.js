3fbda79a056b94e182cd40358572fccc919b6762
Omarjbair <omar.jbair.o@gmail.com>
2023-03-21 23:15:30 +0300

    First Upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let text_input = document.getElementById("Text");
+let button = document.getElementById("btn");
+
+button.addEventListener('click', () => {
+    if(!(text_input.value === "" || text_input.value === " "))
+        sendToChatGPT();
+        text_input.value = ""
+    });
+
+function sendToChatGPT(){
+    
+    let value = document.getElementById("Text").value;
+
+    let body = {
+    model: "text-davinci-003",
+    prompt: value,
+    max_tokens: 200,
+    temperature: 0.7,
+    };
+    let headers = {
+    Authorization:
+        "Bearer API_KEY_HERE",
+    };
+    axios
+    .post("https://api.openai.com/v1/completions", body, {
+    headers: headers,
+    })
+    .then((response) => {
+        let reply = response.data.choices[0].text;
+        document.getElementById("reply").textContent = reply;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {
+        backgroundImage: {
+            'landing_page_image': "url('images/banner-item-01.jpg')"
+        }
+    },
+    },
+    plugins: [],
+}
